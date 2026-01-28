e8c256e0a5dad2bb00ed99c9d803e7f9dcb5de5d
Varun k <varunkuruba12@gmail.com>
2026-01-28 11:17:26 +0530

    Delete breakdemo.java

--- breakdemo.java
+++
@@ -1,8 +0,0 @@
-public class breakdemo{
-    public static void main(String[] args) {
-        for(int i=1;i<=10;i++){
-            if(i==5)break;
-            System.out.println("i="+i);
-        }
-    }
-}
